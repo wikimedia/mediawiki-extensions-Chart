c089f8e85d89c88fc39044db8b9fba7d6fdd0db7
hmonroy <hmonroy@wikimedia.org>
2026-03-21 14:02:19 -0700

    Add `Visual edit` tab to Chart pages
    Introduced feature flag `$wgChartVisualModeEnable` that activates a
    Visual edit tab where users will be able to use a form to setup a chart.
    
    Bug: T419923
    Change-Id: I98e13e0b73fcec749472c8f19ac9b1e6c27fb899

--- src/Hooks.php
+++ src/Hooks.php
@@ -2,14 +2,34 @@
 
 namespace MediaWiki\Extension\Chart;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Deferred\LinksUpdate\LinksUpdate;
 use MediaWiki\Hook\LinksUpdateCompleteHook;
 use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use MediaWiki\Output\OutputPage;
 use MediaWiki\Parser\Parser;
+use MediaWiki\Skin\Skin;
+use MediaWiki\Skin\SkinTemplate;
 use MediaWiki\Storage\Hook\PageSaveCompleteHook;
+use MediaWiki\Title\Title;
 
-class Hooks implements ParserFirstCallInitHook, PageSaveCompleteHook, LinksUpdateCompleteHook {
+class Hooks implements
+	BeforePageDisplayHook,
+	ParserFirstCallInitHook,
+	PageSaveCompleteHook,
+	LinksUpdateCompleteHook,
+	SkinTemplateNavigation__UniversalHook
+{
+	protected bool $isChartVisualModeEnabled;
+
+	public function __construct(
+		private readonly Config $config,
+	) {
+		$this->isChartVisualModeEnabled = $config->get( 'ChartVisualModeEnabled' );
+	}
 
 	public static function onRegistration() {
 		global $wgChartServiceUrl, $wgChartCliPath;
@@ -50,5 +70,78 @@ class Hooks implements ParserFirstCallInitHook, PageSaveCompleteHook, LinksUpdat
 		/** @var ChartMetrics $chartMetrics */
 		$chartMetrics = MediaWikiServices::getInstance()->getService( 'Chart.ChartMetrics' );
 		$chartMetrics->trackChartAddedToPage( $linksUpdate );
+	}
+
+	/**
+	 * Add an "Visual" tab to the navigation of Data: chart.
+	 *
+	 *
+	 * @param SkinTemplate $sktemplate
+	 * @param array &$links
+	 */
+	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ): void {
+		if ( $this->shouldSupportVisualMode( $sktemplate->getTitle() ) ) {
+			$this->updateEditLinks( $sktemplate, $links );
+		}
+	}
+
+	/**
+	 * Adds Chart visual editor JS to the output.
+	 *
+	 * This is attached to the MediaWiki 'BeforePageDisplay' hook.
+	 *
+	 * @param OutputPage $output The page view.
+	 * @param Skin $skin The skin that's going to build the UI.
+	 */
+	public function onBeforePageDisplay( $output, $skin ): void {
+		if ( $this->shouldSupportVisualMode( $output->getTitle() ) && $output->getRequest()->getBool( 'visual' ) ) {
+			$output->addModules( 'ext.chart.visualEditMode' );
+		}
+	}
+
+	/**
+	 * Update the edit links for the given skin template.
+	 *
+	 * @param SkinTemplate $sktemplate
+	 * @param array &$links
+	 */
+	private function updateEditLinks( SkinTemplate $sktemplate, array &$links ): void {
+		$setVisualMode = $sktemplate->getRequest()->getInt( 'visual' );
+		// Deselect "edit source" link
+		if ( $setVisualMode ) {
+			$links['views']['edit']['class'] = '';
+		}
+		$chartVisualEditTab = [
+			'text' => $sktemplate->msg( 'chart-visualedit-tab-label' )->text(),
+			'icon' => 'edit',
+			'class' => $setVisualMode ? 'selected' : '',
+			'href' => $sktemplate->getRelevantTitle()->getLinkURL( [ 'action' => 'edit', 'visual' => 1 ] )
+		];
+
+		$tabs = $links['views'];
+		// Attempt to insert before the "View history" tab.
+		$newTabs = [];
+		foreach ( $tabs as $key => $val ) {
+			if ( $key === 'history' ) {
+				$newTabs['chart-visual-edit'] = $chartVisualEditTab;
+			}
+			$newTabs[$key] = $val;
+		}
+		// If no "View history" tab was found, append to the end.
+		if ( !isset( $newTabs['chart-visual-edit'] ) ) {
+			$newTabs['chart-visual-edit'] = $chartVisualEditTab;
+		}
+
+		$links['views'] = $newTabs;
+	}
+
+	/**
+	 * Determine if Visual mode should be added to the given page title
+	 *
+	 * @param Title $title Title to render visual mode
+	 * @return bool True if chart visual mode flag is enabled and it's a chart page
+	 */
+	private function shouldSupportVisualMode( Title $title ): bool {
+		return $this->isChartVisualModeEnabled && $title->getContentModel() === JCChartContent::CONTENT_MODEL;
 	}
 }
